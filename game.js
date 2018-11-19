c99a1e4be7eb3a1c4bb09f19e3ea1d6a86440482
Sam Perumal <sam_lki@yahoo.com>
2018-11-19 15:37:23 +0200

    Added comments.

--- game.js
+++ game.js
@@ -1,20 +1,31 @@
 var gameState = {};
 
+// Called on game startup
 function initialise() {
+    // Setup global game state
     gameState = {
+        // Display state
         gfx: {
             cellSize: 25,
         },
+        // Player state
         player: {
             x: 0,
             y: 0
-        }
+        },
+        // Map data
+        mapData : null,
+        // Convenience copy of above for display
+        mapArray: null,
     };
 
+    // Setup dictionary mapping symbols to css classes
     initClassMap();
+    // Capture display elements drawing/update operations
     gameState.gfx.svg = d3.select("#gfx");
     gameState.gfx.floor = gameState.gfx.svg.append("g").attr("class", "floor");
 
+    // Call promise chain to load and draw map from file
     loadMap()
         .then(draw)
         .then(update)
@@ -27,32 +38,39 @@ function initialise() {
         });
 }
 
+// Draw map when first loaded
 function draw() {
     console.log("Drawing map");
 
     var gfx = gameState.gfx;
 
+    // Bind cell data to SVG groups
     var allCells = gfx.floor.selectAll("g.cell")
         .data(gameState.mapArray, d => d.i);
 
+    // Create display elements for new data
     allCells.enter()
         .append("g").attr("class", "cell")
         .attr("transform", d => "translate(" + (d.x * gfx.cellSize) + "," + (d.y * gfx.cellSize) + ")")
         .append("text")
         .attr("transform", d => "translate(" + (gfx.cellSize / 2) + "," + (gfx.cellSize / 2) + ")");
 
+    // Remove display elements for removed elements
     allCells.exit().remove();
 }
 
+// Update map after actions
 function update() {
     console.log("Updating map");
     var gfx = gameState.gfx;
 
+    // Update cell css class and text symbol
     gfx.floor.selectAll("g.cell text")
         .attr("class", d => gfx.colors[d.s()])
         .text(d => d.s());
 }
 
+// Process key input
 function processInput(d) {
     switch (d3.event.code) {
         case "KeyW": requestMove(0, -1); break;
@@ -62,11 +80,14 @@ function processInput(d) {
         default: return;
     }
 
+    // Clear textbox
     d3.select("#in").property("value", "");
 
+    // Redraw map
     update();
 }
 
+// Attempt to move to new cell
 function requestMove(x, y) {
     var player = gameState.player;
     var currentCell = gameState.mapData[player.y][player.x];
@@ -76,6 +97,7 @@ function requestMove(x, y) {
     if (gameState.mapData[player.y + y] != null && gameState.mapData[player.y + y][player.x + x] != null) {
         var proposedCell = gameState.mapData[player.y + y][player.x + x];
         
+        // Find handler for target destination based on it's displayed symbol
         var action = possibleDestinations[proposedCell.s()];
         if (action != null) {
             console.log("Processing action");
@@ -84,6 +106,7 @@ function requestMove(x, y) {
         }
     }
 
+    // Update/clear the error message
     d3.select("#error").text(errorMessage);
 }
 
@@ -91,6 +114,7 @@ var possibleDestinations = {
     ".": moveToEmptySpace,
 };
 
+// Simplest action function - just move the player to the new cell
 function moveToEmptySpace(currentCell, proposedCell) {
     currentCell.p = false;
     proposedCell.p = true;
@@ -98,6 +122,7 @@ function moveToEmptySpace(currentCell, proposedCell) {
     gameState.player.y = proposedCell.y;
 }
 
+// Create symbol to css class map
 function initClassMap() {
     var gfx = gameState.gfx;
     gfx.colors = {
