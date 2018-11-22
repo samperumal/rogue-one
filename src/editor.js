04801409a1c1facbd8f669ea0daf2e27d9afb774
Sam Perumal <sam_lki@yahoo.com>
2018-11-22 17:59:56 +0200

    Click to set cell contents

--- src/editor.js
+++ src/editor.js
@@ -7,7 +7,10 @@ class editor {
             cellSize: 25,
             width: 1200,
             height: 450
-        }
+        };
+
+        this.mapData = [];
+        this.mapArray = [];
     }
 
     initialise() {
@@ -15,6 +18,85 @@ class editor {
         this.gfx.pan = this.gfx.svg.append("g").attr("class", "pan");
         this.gfx.grid = this.gfx.pan.append("g").attr("class", "grid");
 
-        console.log("Initialised");
+        this.createGrid();
+
+        this.updateGrid();
+    }
+
+    createGrid() {
+        const width = +d3.select("#width").property("value");
+        const height = +d3.select("#height").property("value");
+
+        this.mapData = [];
+        for (let y = 0; y < height; ++y) {
+            const row = [];
+            for (let x = 0; x < width; ++x) {
+                let c = new Cell(x, y);
+                //c.editor = this;
+                row.push(c);
+            }
+            this.mapData.push(row);
+        }
+
+        this.mapArray = this.mapData.reduce((a, b) => a.concat(b), []);
+
+        this.drawGrid();
+    }
+
+    drawGrid() {
+        const cellSize = this.gfx.cellSize;
+        const margin = 2;
+
+        // Clear all existing groups
+        this.gfx.grid.selectAll("g.cell").remove();
+
+        const cells = this.gfx.grid.selectAll("g.cell")
+            .data(this.mapArray);
+
+        // Necessary to maintain parent reference across events
+        const obj = this;
+
+        // Create new groups and populate with contents
+        const newCells = cells.enter()
+            .append("g")
+            .attr("class", "cell")
+            .attr("transform", d => "translate(" + (d.x * cellSize) + "," + (d.y * cellSize) + ")")            
+            .on("mouseenter", this.cellMouseEnter)
+            .on("click", d => this.cellClick(d, obj));
+
+        newCells
+            .append("text")
+            .attr("x", cellSize / 2)
+            .attr("y", cellSize / 2);
+
+        newCells
+            .append("rect")
+            .attr("class", "cellbg")
+            .attr("x", margin)
+            .attr("y", margin)
+            .attr("width", cellSize - margin)
+            .attr("height", cellSize - margin);
+
+        
+    }
+
+    updateGrid() {
+        const cells = this.gfx.grid.selectAll("g.cell");
+
+        cells.select("text")
+            .text(d => d.s())
+            .attr("class", d => d.css());
+    }
+
+    cellMouseEnter(data) {
+        d3.select("#json-properties").text(JSON.stringify(data, null, 2));        
+    }
+
+    cellClick(data, obj) {
+        data.t = "#";
+        data.tt = TILES["#"].tt;
+
+        obj.updateGrid();
+        obj.cellMouseEnter(data);
     }
 }
