210f5165903adf712ba2bd68a26902c480649703
Murray <mjrmua@gmail.com>
2018-11-25 07:51:34 +0200

    Add additional movement tests
    And watch them conflict with eachother

--- test/movment.spec.ts
+++ test/movment.spec.ts
@@ -17,8 +17,33 @@ describe("Test chamber 1 - Movement:",()=>{
         initialise(Promise.resolve(chamber))
         .then(done)
     });
+
     it("player starts at (2,3)",()=>
         expect([gameState.player.x, gameState.player.y])
             .toEqual([2,3])
     )
+
+    it('ArrowLeft will move player to (1,3)', ()=> {
+        processInput("ArrowLeft");
+        expect([gameState.player.x, gameState.player.y])
+            .toEqual([1,3])
+    })
+
+    it('ArrowRight will move player to (3,3)', ()=> {
+        processInput("ArrowRight");
+        expect([gameState.player.x, gameState.player.y])
+            .toEqual([3,3])
+    })
+
+    it('ArrowUp will move player to (2,2)', ()=> {
+        processInput("ArrowUp");
+        expect([gameState.player.x, gameState.player.y])
+            .toEqual([2,2])
+    })
+
+    fit('ArrowDown will move player to (2,4)', ()=> {
+        processInput("ArrowDown");
+        expect([gameState.player.x, gameState.player.y])
+            .toEqual([2,4])
+    })
 });
